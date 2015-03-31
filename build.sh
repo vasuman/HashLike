dc4ee28c2a17c92ea5415e6f1a37de0d0305d384
Vasuman R <vasumanar@gmail.com>
2015-03-31 09:14:32 -0400

    Fixed build script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ mkdir -p out/static
 
 go build -o out/server.exe .
 
-emcc  client/hc_worker.c --pre-js=client/hc-worker.js -o out/static/hc-worker-emc.js -s EXPORTED_FUNCTIONS="['_get_nonce']"
+emcc  client/hc_worker.c --pre-js client/hc-worker.js -o out/static/hc-worker-emc.js -s EXPORTED_FUNCTIONS="['_get_nonce']"
 cp client/hashlike.js out/static/
 
 
