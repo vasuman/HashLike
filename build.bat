31b79ffc1624c83c9967b579a98cdb44a3c9fa98
Vasuman R <vasumanar@gmail.com>
2015-04-06 18:13:31 +0530

    Using `call` for forking new processes

--- build.bat
+++ build.bat
@@ -3,11 +3,12 @@
 mkdir out
 mkdir out\static
 
-go build -o out\server.exe .
+call go build -o out\server.exe .
 
-emcc  client\hc_worker.c --pre-js client\hc-worker.js -o out\static\hc-worker-emc.js -s EXPORTED_FUNCTIONS="['_get_nonce']"
+xcopy .\client\hashlike.js .\out\static\ /Y
 
-copy .\client\hashlike.js .\out\static\hashlike.js
+call emcc client\hc_worker.c --pre-js client\hc-worker.js -o out\static\hc-worker-emc.js -s EXPORTED_FUNCTIONS="['_get_nonce']"
+
 
 
 
